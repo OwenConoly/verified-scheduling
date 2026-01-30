6eaa1d2fc19ffbd3de7f4f6e7a4a72a6b883d833
Owen Conoly <otconoly@hotmail.com>
2026-01-30 15:47:37 -0500

    try to fix Reify

--- src/verified_lowering/inferpad/Reify.v
+++ src/verified_lowering/inferpad/Reify.v
@@ -26,15 +26,28 @@ Open Scope string_scope.
 (*where did this come from?  did i put it here?*)
 Set Default Proof Mode "Classic".
 
-Definition pExpr_type var (t : type) : Type :=
+Inductive type' : Type :=
+| tZ'
+| tB'
+| tensor_n' (n : nat).
+
+Definition pExpr_type var (t : type') : Type :=
   match t with
-  | tZ => pZexpr (var tZ)
-  | tB => pBexpr (var tZ)
-  | tensor_n n => pATLexpr var n
+  | tZ' => pZexpr (var tZarg) (var tZiter)
+  | tB' => pBexpr (var tZarg) (var tZiter)
+  | tensor_n' n => pATLexpr var n
   end.
 
-Definition pair_to_reify (f : (type -> Type) -> Type) : Type :=
-  f interp_type * (forall var, f (pExpr_type var)).
+Print interp_type.
+Definition interp_type' t : Type :=
+  match t with
+  | tZ' => Z
+  | tB' => bool
+  | tensor_n' n => dim_n n
+  end.
+
+Definition pair_to_reify (f : (type' -> Type) -> Type) : Type :=
+  f interp_type' * (forall var, f (pExpr_type var)).
 
 Definition gen_n n := @genr (dim_n n) _.
 Definition sum_n n := @sumr (dim_n n) _.
@@ -54,67 +67,67 @@ Definition let_nm n m := @let_binding (dim_n n) (dim_n m).
 Notation "[> <]" := tt (format "[> <]").
 Notation "[> x <]" := (x, tt).
 Notation "[> x ; y ; .. ; z <]" := ((x, (y, .. (z, tt) ..))).
-
+Print pZexpr.
 Definition pairs_to_reify :=
   [>
      (Z0, fun var => ZZ0)
-    : pair_to_reify (fun var => var tZ);
+    : pair_to_reify (fun var => var tZ');
    (Zpos, fun var => ZZpos)
-     : pair_to_reify (fun var => positive -> var tZ);
+     : pair_to_reify (fun var => positive -> var tZ');
    (Zneg, fun var => ZZneg)
-     : pair_to_reify (fun var => positive -> var tZ);
+     : pair_to_reify (fun var => positive -> var tZ');
    (Z.opp, fun var => ZZopp)
-     : pair_to_reify (fun var => var tZ -> var tZ);
+     : pair_to_reify (fun var => var tZ' -> var tZ');
    (IZR, fun var => SIZR)
-     : pair_to_reify (fun var => var tZ -> var (tensor_n O));
-   (gen_n, fun var => (fun n lo hi body => @Gen var n lo hi (fun x => body (@ZVar (var tZ) x))))
-     : pair_to_reify (fun var => forall n, var tZ -> var tZ -> (var tZ -> var (tensor_n n)) -> var (tensor_n (S n)));
-   (sum_n, fun var => (fun n lo hi body => @Sum var n lo hi (fun x => body (@ZVar (var tZ) x))))
-     : pair_to_reify (fun var => forall n, var tZ -> var tZ -> (var tZ -> var (tensor_n n)) -> var (tensor_n n));
+     : pair_to_reify (fun var => var tZ' -> var (tensor_n' O));
+   (gen_n, fun var => (fun n lo hi body => @Gen var n lo hi (fun x => body (ZIterVar x))))
+     : pair_to_reify (fun var => forall n, var tZ' -> var tZ' -> (var tZ' -> var (tensor_n' n)) -> var (tensor_n' (S n)));
+   (sum_n, fun var => (fun n lo hi body => @Sum var n lo hi (fun x => body (ZIterVar x))))
+     : pair_to_reify (fun var => forall n, var tZ' -> var tZ' -> (var tZ' -> var (tensor_n' n)) -> var (tensor_n' n));
    (let_nm, fun var => (fun n m x f => @Lbind var n m x (fun x0 => f (@Var var n x0))))
-     : pair_to_reify (fun var => forall n m, var (tensor_n n) -> (var (tensor_n n) -> var (tensor_n m)) -> var (tensor_n m));
+     : pair_to_reify (fun var => forall n m, var (tensor_n' n) -> (var (tensor_n' n) -> var (tensor_n' m)) -> var (tensor_n' m));
    (@gget_R, fun var => @Get var)
-     : pair_to_reify (fun var => forall n, var (tensor_n n) -> list (var tZ) -> var (tensor_n O));
+     : pair_to_reify (fun var => forall n, var (tensor_n' n) -> list (var tZ') -> var (tensor_n' O));
    (iverson_n, fun var => @Guard var)
-     : pair_to_reify (fun var => forall n, var tB -> var (tensor_n n) -> var (tensor_n n));
+     : pair_to_reify (fun var => forall n, var tB' -> var (tensor_n' n) -> var (tensor_n' n));
    (flatten_n, fun var => @Flatten var)
-     : pair_to_reify (fun var => forall n, var (tensor_n (S (S n))) -> var (tensor_n (S n)));
+     : pair_to_reify (fun var => forall n, var (tensor_n' (S (S n))) -> var (tensor_n' (S n)));
    (truncr_n, fun var => @Truncr var)
-     : pair_to_reify (fun var => forall n, nat -> var (tensor_n (S n)) -> var (tensor_n (S n)));
+     : pair_to_reify (fun var => forall n, nat -> var (tensor_n' (S n)) -> var (tensor_n' (S n)));
    (truncl_n, fun var => @Truncl var)
-     : pair_to_reify (fun var => forall n, nat -> var (tensor_n (S n)) -> var (tensor_n (S n)));
+     : pair_to_reify (fun var => forall n, nat -> var (tensor_n' (S n)) -> var (tensor_n' (S n)));
    (transpose_n, fun var => @Transpose var)
-     : pair_to_reify (fun var => forall n, var (tensor_n (S (S n))) -> var (tensor_n (S (S n))));
+     : pair_to_reify (fun var => forall n, var (tensor_n' (S (S n))) -> var (tensor_n' (S (S n))));
    (concat_n, fun var => @Concat var)
-     : pair_to_reify (fun var => forall n, var (tensor_n (S n)) -> var (tensor_n (S n)) -> var (tensor_n (S n)));
+     : pair_to_reify (fun var => forall n, var (tensor_n' (S n)) -> var (tensor_n' (S n)) -> var (tensor_n' (S n)));
    (tile_n, fun var => (fun n x k => @Split var n k x))
-     : pair_to_reify (fun var => forall n, var (tensor_n (S n)) -> nat -> var (tensor_n (S (S n))));
+     : pair_to_reify (fun var => forall n, var (tensor_n' (S n)) -> nat -> var (tensor_n' (S (S n))));
    (Z.of_nat, fun var => ZZ_of_nat)
-     : pair_to_reify (fun var => nat -> var tZ);
+     : pair_to_reify (fun var => nat -> var tZ');
    (Z.add, fun var => ZBop ZPlus)
-     : pair_to_reify (fun var => var tZ -> var tZ -> var tZ);
+     : pair_to_reify (fun var => var tZ' -> var tZ' -> var tZ');
    (Z.sub, fun var => ZBop ZMinus)
-     : pair_to_reify (fun var => var tZ -> var tZ -> var tZ);
+     : pair_to_reify (fun var => var tZ' -> var tZ' -> var tZ');
    (Z.mul, fun var => ZBop ZTimes)
-     : pair_to_reify (fun var => var tZ -> var tZ -> var tZ);
+     : pair_to_reify (fun var => var tZ' -> var tZ' -> var tZ');
    (Z.div, fun var => ZBop ZDivf)
-     : pair_to_reify (fun var => var tZ -> var tZ -> var tZ);
+     : pair_to_reify (fun var => var tZ' -> var tZ' -> var tZ');
    (div_ceil, fun var => ZBop ZDivc)
-     : pair_to_reify (fun var => var tZ -> var tZ -> var tZ);
+     : pair_to_reify (fun var => var tZ' -> var tZ' -> var tZ');
    (Z.modulo, fun var => ZBop ZMod)
-     : pair_to_reify (fun var => var tZ -> var tZ -> var tZ);
+     : pair_to_reify (fun var => var tZ' -> var tZ' -> var tZ');
    (Z.ltb, fun var => BBop BLt)
-     : pair_to_reify (fun var => var tZ -> var tZ -> var tB);
+     : pair_to_reify (fun var => var tZ' -> var tZ' -> var tB');
    (Z.leb, fun var => BBop BLe)
-     : pair_to_reify (fun var => var tZ -> var tZ -> var tB);
+     : pair_to_reify (fun var => var tZ' -> var tZ' -> var tB');
    (Z.eqb, fun var => BBop BEq)
-     : pair_to_reify (fun var => var tZ -> var tZ -> var tB);
+     : pair_to_reify (fun var => var tZ' -> var tZ' -> var tB');
    (andb, fun var => BAnd)
-     : pair_to_reify (fun var => var tB -> var tB -> var tB);
+     : pair_to_reify (fun var => var tB' -> var tB' -> var tB');
    (Rmult, fun var => @SBop var Mul)
-     : pair_to_reify (fun var => var (tensor_n O) -> var (tensor_n O) -> var (tensor_n O));
+     : pair_to_reify (fun var => var (tensor_n' O) -> var (tensor_n' O) -> var (tensor_n' O));
    (Rplus, fun var => SBop Add)
-     : pair_to_reify (fun var => var (tensor_n O) -> var (tensor_n O) -> var (tensor_n O))
+     : pair_to_reify (fun var => var (tensor_n' O) -> var (tensor_n' O) -> var (tensor_n' O))
        <].
 Class TupleMap_fst T := { tuplemap_fst_Type : Type; tuplemap_fst : T -> tuplemap_fst_Type }.
 Instance TupleMap_fst_nil : TupleMap_fst unit := { tuplemap_fst := fun x => x }.
@@ -129,7 +142,7 @@ Instance TupleMap_app_nil U : TupleMap_app U unit := { tuplemap_app := fun _ x =
 Instance TupleMap_app_cons U B C {X: TupleMap_app U C} : TupleMap_app U ((forall U, B U) * C) := { tuplemap_app := fun u '(a, c) => (a u, tuplemap_app u c) }.
 
 Definition shallows :=
-  ltac:(let y := eval cbn -[interp_type] in (tuplemap_fst pairs_to_reify) in exact y).
+  ltac:(let y := eval cbn -[interp_type'] in (tuplemap_fst pairs_to_reify) in exact y).
 Definition deeps :=
   ltac:(let y := eval simpl in (tuplemap_snd pairs_to_reify) in exact y).
 
@@ -170,68 +183,78 @@ Ltac pattern_shallows x :=
     (*copy-paste result of "print_shallows" on following lines*)
     (*TODO is there a less dumb way to do this?  Ltac metaprogramming?*)
 
-,( 0%Z : (interp_type tZ) )
-,( Z.pos : (positive -> interp_type tZ) )
-,( Z.neg : (positive -> interp_type tZ) )
-,( Z.opp : (interp_type tZ -> interp_type tZ) )
-,( IZR : (interp_type tZ -> interp_type (tensor_n 0)) )
+,( 0%Z : (interp_type' tZ') )
+,( Z.pos : (positive -> interp_type' tZ') )
+,( Z.neg : (positive -> interp_type' tZ') )
+,( Z.opp : (interp_type' tZ' -> interp_type' tZ') )
+,( IZR : (interp_type' tZ' -> interp_type' (tensor_n' 0)) )
 ,( gen_n :
 (forall n : nat,
- interp_type tZ ->
- interp_type tZ ->
- (interp_type tZ -> interp_type (tensor_n n)) -> interp_type (tensor_n (S n)))
+ interp_type' tZ' ->
+ interp_type' tZ' ->
+ (interp_type' tZ' -> interp_type' (tensor_n' n)) -> interp_type' (tensor_n' (S n)))
 )
 ,( sum_n :
 (forall n : nat,
- interp_type tZ ->
- interp_type tZ ->
- (interp_type tZ -> interp_type (tensor_n n)) -> interp_type (tensor_n n))
+ interp_type' tZ' ->
+ interp_type' tZ' ->
+ (interp_type' tZ' -> interp_type' (tensor_n' n)) -> interp_type' (tensor_n' n))
 )
 ,( let_nm :
 (forall n m : nat,
- interp_type (tensor_n n) ->
- (interp_type (tensor_n n) -> interp_type (tensor_n m)) -> interp_type (tensor_n m))
+ interp_type' (tensor_n' n) ->
+ (interp_type' (tensor_n' n) -> interp_type' (tensor_n' m)) ->
+ interp_type' (tensor_n' m))
 )
 ,( @gget_R :
 (forall n : nat,
- interp_type (tensor_n n) -> list (interp_type tZ) -> interp_type (tensor_n 0))
+ interp_type' (tensor_n' n) -> list (interp_type' tZ') -> interp_type' (tensor_n' 0))
 )
 ,( iverson_n :
-(forall n : nat, interp_type tB -> interp_type (tensor_n n) -> interp_type (tensor_n n))
+(forall n : nat,
+ interp_type' tB' -> interp_type' (tensor_n' n) -> interp_type' (tensor_n' n))
 )
 ,( flatten_n :
-(forall n : nat, interp_type (tensor_n (S (S n))) -> interp_type (tensor_n (S n))) )
+(forall n : nat, interp_type' (tensor_n' (S (S n))) -> interp_type' (tensor_n' (S n)))
+)
 ,( truncr_n :
-(forall n : nat, nat -> interp_type (tensor_n (S n)) -> interp_type (tensor_n (S n))) )
+(forall n : nat,
+ nat -> interp_type' (tensor_n' (S n)) -> interp_type' (tensor_n' (S n)))
+)
 ,( truncl_n :
-(forall n : nat, nat -> interp_type (tensor_n (S n)) -> interp_type (tensor_n (S n))) )
+(forall n : nat,
+ nat -> interp_type' (tensor_n' (S n)) -> interp_type' (tensor_n' (S n)))
+)
 ,( transpose_n :
-(forall n : nat, interp_type (tensor_n (S (S n))) -> interp_type (tensor_n (S (S n))))
+(forall n : nat,
+ interp_type' (tensor_n' (S (S n))) -> interp_type' (tensor_n' (S (S n))))
 )
 ,( concat_n :
 (forall n : nat,
- interp_type (tensor_n (S n)) ->
- interp_type (tensor_n (S n)) -> interp_type (tensor_n (S n)))
+ interp_type' (tensor_n' (S n)) ->
+ interp_type' (tensor_n' (S n)) -> interp_type' (tensor_n' (S n)))
 )
 ,( tile_n :
 (forall n : nat,
- interp_type (tensor_n (S n)) -> nat -> interp_type (tensor_n (S (S n))))
+ interp_type' (tensor_n' (S n)) -> nat -> interp_type' (tensor_n' (S (S n))))
 )
-,( Z.of_nat : (nat -> interp_type tZ) )
-,( Z.add : (interp_type tZ -> interp_type tZ -> interp_type tZ) )
-,( Z.sub : (interp_type tZ -> interp_type tZ -> interp_type tZ) )
-,( Z.mul : (interp_type tZ -> interp_type tZ -> interp_type tZ) )
-,( Z.div : (interp_type tZ -> interp_type tZ -> interp_type tZ) )
-,( div_ceil : (interp_type tZ -> interp_type tZ -> interp_type tZ) )
-,( Z.modulo : (interp_type tZ -> interp_type tZ -> interp_type tZ) )
-,( Z.ltb : (interp_type tZ -> interp_type tZ -> interp_type tB) )
-,( Z.leb : (interp_type tZ -> interp_type tZ -> interp_type tB) )
-,( Z.eqb : (interp_type tZ -> interp_type tZ -> interp_type tB) )
-,( andb : (interp_type tB -> interp_type tB -> interp_type tB) )
+,( Z.of_nat : (nat -> interp_type' tZ') )
+,( Z.add : (interp_type' tZ' -> interp_type' tZ' -> interp_type' tZ') )
+,( Z.sub : (interp_type' tZ' -> interp_type' tZ' -> interp_type' tZ') )
+,( Z.mul : (interp_type' tZ' -> interp_type' tZ' -> interp_type' tZ') )
+,( Z.div : (interp_type' tZ' -> interp_type' tZ' -> interp_type' tZ') )
+,( div_ceil : (interp_type' tZ' -> interp_type' tZ' -> interp_type' tZ') )
+,( Z.modulo : (interp_type' tZ' -> interp_type' tZ' -> interp_type' tZ') )
+,( Z.ltb : (interp_type' tZ' -> interp_type' tZ' -> interp_type' tB') )
+,( Z.leb : (interp_type' tZ' -> interp_type' tZ' -> interp_type' tB') )
+,( Z.eqb : (interp_type' tZ' -> interp_type' tZ' -> interp_type' tB') )
+,( andb : (interp_type' tB' -> interp_type' tB' -> interp_type' tB') )
 ,( Rmult :
-(interp_type (tensor_n 0) -> interp_type (tensor_n 0) -> interp_type (tensor_n 0)) )
+(interp_type' (tensor_n' 0) -> interp_type' (tensor_n' 0) -> interp_type' (tensor_n' 0))
+)
 ,( Rplus :
-(interp_type (tensor_n 0) -> interp_type (tensor_n 0) -> interp_type (tensor_n 0)) )
+(interp_type' (tensor_n' 0) -> interp_type' (tensor_n' 0) -> interp_type' (tensor_n' 0))
+)
 
     in x.
 
@@ -243,14 +266,14 @@ Ltac get_fun x :=
 
 (*assumes that the reification target appears in the goal*)
 Ltac make_types_reifiable_in x :=
-  change R with (interp_type (tensor_n O)) in x;
-  repeat change (list (interp_type (tensor_n ?n))) with (interp_type (tensor_n (S n))) in x;
+  change R with (interp_type (tensor_n' O)) in x;
+  repeat change (list (interp_type (tensor_n' ?n))) with (interp_type (tensor_n' (S n))) in x;
   change RTensorElem with (dim_n_TensorElem O) in x;
   repeat change (@TensorTensorElem _ (dim_n_TensorElem ?n)) with
     (dim_n_TensorElem (S n)) in x;
   repeat change (@get _ _ ?v ?i) with (@gget_R (S O) v [i]) in x;
   repeat change (@gget_R ?n (@get _ _ ?v ?idx) ?idxs) with (@gget_R (S n) v (idx :: idxs)) in x;
-  change Z with (interp_type tZ) in x;
+  change Z with (interp_type tZ') in x;
   cbv [gen sum Common.Truncr] in x;
   (*Z's are not allowed to be used as constants;
     in particular, they cannot be used to define nats*)
@@ -259,17 +282,17 @@ Ltac make_types_reifiable_in x :=
     | x := context[(Z.to_nat ?y)] |- _ =>
         let k := fresh "k" in set (k := Z.to_nat y)
     end;
-  repeat change (@genr (interp_type (tensor_n ?n)) _) with (gen_n n) in x;
-  repeat change (@sumr (interp_type (tensor_n ?n)) _) with (sum_n n) in x;
-  repeat change (@iverson (interp_type (tensor_n ?n)) _) with (iverson_n n) in x;
-  repeat change (@Common.flatten (interp_type (tensor_n ?n)) _) with (flatten_n n) in x;
-  repeat change (@truncr (interp_type (tensor_n ?n)) _) with (truncr_n n) in x;
-  repeat change (@truncl (interp_type (tensor_n ?n)) _) with (truncl_n n) in x;
-  repeat change (@transpose (interp_type (tensor_n ?n)) _) with (transpose_n n) in x;
-  repeat change (@concat (interp_type (tensor_n ?n)) _) with (concat_n n) in x;
-  repeat change (@tile (interp_type (tensor_n ?n)) _) with (tile_n n) in x;
-  repeat change (@let_binding (interp_type (tensor_n ?n)) (interp_type (tensor_n ?m))) with (let_nm n m) in x;
-  change (@bin (interp_type (tensor_n O)) _) with Rplus in x.
+  repeat change (@genr (interp_type (tensor_n' ?n)) _) with (gen_n n) in x;
+  repeat change (@sumr (interp_type (tensor_n' ?n)) _) with (sum_n n) in x;
+  repeat change (@iverson (interp_type (tensor_n' ?n)) _) with (iverson_n n) in x;
+  repeat change (@Common.flatten (interp_type (tensor_n' ?n)) _) with (flatten_n n) in x;
+  repeat change (@truncr (interp_type (tensor_n' ?n)) _) with (truncr_n n) in x;
+  repeat change (@truncl (interp_type (tensor_n' ?n)) _) with (truncl_n n) in x;
+  repeat change (@transpose (interp_type (tensor_n' ?n)) _) with (transpose_n n) in x;
+  repeat change (@concat (interp_type (tensor_n' ?n)) _) with (concat_n n) in x;
+  repeat change (@tile (interp_type (tensor_n' ?n)) _) with (tile_n n) in x;
+  repeat change (@let_binding (interp_type (tensor_n' ?n)) (interp_type (tensor_n' ?m))) with (let_nm n m) in x;
+  change (@bin (interp_type (tensor_n' O)) _) with Rplus in x.
 
 Ltac Reify x name :=
   set (y := x);
